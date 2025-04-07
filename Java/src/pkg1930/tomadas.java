1d515afe6ca0cae76d79a0cb1da64a25a05cfe28
Lucas Araujo <Luc4s_santos.s@protonmail.com>
2025-04-07 15:03:45 -0300

    up

---
+++ Java/src/pkg1930/tomadas.java
@@ -0,0 +1,25 @@
+package pkg1930;
+
+import java.util.Scanner;
+
+public class tomadas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] tomadas = new int[4];
+        int soma = 0;
+        int maxAparelhos = 0;
+
+        for (int i = 0; i < 4; i++){
+            tomadas[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < tomadas.length; i++){
+            soma += tomadas[i];
+        }
+
+        maxAparelhos = soma - 3;
+
+        System.out.println(maxAparelhos);
+    }
+}
